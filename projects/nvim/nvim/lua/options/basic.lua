c4dab4db29a6a9cda2e6628c8896daa6bff98dee
allavinner <joel.w.ottosson@gmail.com>
2024-09-13 21:58:32 +0200

    Make buffer/pane movement depend on if we are in a tmux session or not

--- projects/nvim/nvim/lua/options/basic.lua
+++ projects/nvim/nvim/lua/options/basic.lua
@@ -93,7 +93,7 @@ vim.keymap.set("n", "<leader>eq", vim.diagnostic.setloclist, { desc = "Open diag
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set("t", "<C-n>", "<C-\\><C-n>", { desc = "Exit terminal mode. Works in tmux." })
+vim.keymap.set("t", "<C-c>", "<C-\\><C-n>", { desc = "Exit terminal mode. Works in tmux." })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -106,10 +106,12 @@ vim.keymap.set("t", "<C-n>", "<C-\\><C-n>", { desc = "Exit terminal mode. Works 
 --
 --  See `:help wincmd` for a list of all window commands
 --  MIGHT NEED TO BE UNCOMMENTED IF WE ARE NOT IN TMUX ...
--- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
--- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
--- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
--- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+if not os.getenv("TMUX") then
+	vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+	vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+	vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+	vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+end
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
