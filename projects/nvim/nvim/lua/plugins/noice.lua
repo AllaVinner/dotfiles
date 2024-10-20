832bc9a5dab1985f956ce184e2cc7f9f063c756f
allavinner <joel.w.ottosson@gmail.com>
2024-10-20 14:09:21 +0200

    Added Noice plugin for a nice cmd line

---
+++ projects/nvim/nvim/lua/plugins/noice.lua
@@ -0,0 +1,20 @@
+-- Noice, nice looking command line floating window
+local m = {}
+
+m.default = {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+}
+
+return m
