a691d3775b0c7f98a747c0d32c2a1ba6c2eab4db
allavinner <joel.w.ottosson@gmail.com>
2024-10-20 14:10:15 +0200

    Added plugin autopair, for qoutes and brackets

--- projects/nvim/nvim/lua/plugins/autopairs.lua
+++ projects/nvim/nvim/lua/plugins/autopairs.lua
@@ -1,3 +1,6 @@
+-- Autopairs brackets, quotes,
+-- - Creates the corresponding character when insertiing the first
+-- - When creating new line inside, it correctly indent and so on
 return {
 	default = {
 		"windwp/nvim-autopairs",
